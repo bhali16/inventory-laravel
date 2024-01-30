ae9e06f5fcf63eee35c086212fbe040fe3738862
Waqar Ahmad Khan <waqarbinmuhammad@gmail.com>
2024-01-30 11:23:00 +0500

    Removed test route

--- routes/web.php
+++ routes/web.php
@@ -57,3 +57,8 @@ Route::group(['prefix' => 'profile', 'as' => 'profile.', 'namespace' => 'Auth', 
         Route::post('profile/destroy', 'ChangePasswordController@destroy')->name('password.destroyProfile');
     }
 });
+
+Route::get('test', function(){
+    //php info
+    return phpinfo();
+});
